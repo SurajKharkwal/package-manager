fc24c832e63c4b89242fa3044cf3fa3a9791e4f4
SurajKharkwal <kharkwalsuraj13@gmail.com>
2025-04-03 02:14:08 +0530

    Landing Page

---
+++ apps/landing/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows all external images
+      },
+    ],
+  },
+};
+
+export default nextConfig;
