8328267492b703d490489922c33e06ead26c819e
SurajKharkwal <kharkwalsuraj13@gmail.com>
2025-04-27 14:06:33 +0530

    Home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,7 @@
+import { useRouter } from "nextjs-toploader/app";
+
 export default async function Page() {
-  return <div className="">{"Home page"}</div>;
+  const router = useRouter();
+  router.push("https://pck-mgr-landing.vercel.app/");
+  return <div className="">{"Please wait"}</div>;
 }
